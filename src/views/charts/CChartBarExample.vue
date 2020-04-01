c68ff0c81379bbb8c7d3723a4ccaa3ee4a93c4ac
James Morgan <james.morgan@blockrocket.tech>
2020-04-01 21:37:22 +0100

    Adding monthly stats

--- src/views/charts/CChartBarExample.vue
+++ src/views/charts/CChartBarExample.vue
@@ -1,7 +1,7 @@
 <template>
   <CChartBar
     :datasets="defaultDatasets"
-    :labels="['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb']"
+    :labels="['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar']"
   />
 </template>
 
@@ -15,10 +15,15 @@ export default {
     defaultDatasets () {
       return [
         {
-          label: 'primary sales in USD',
+          label: 'Primary sales (USD)',
           backgroundColor: '#5165FF',
-          data: [482, 2103, 1154, 939, 416, 472, 5462, 3230, 4488, 5286, 24861, 32370, 38540],
-        }
+          data: [482, 2103, 1154, 939, 416, 472, 5462, 3230, 4488, 5286, 24861, 32370, 38540, 21742],
+        },
+        // {
+        //   label: 'Secondary sale (USD)',
+        //   backgroundColor: '#ffadc1',
+        //   data: [482, 2103, 1154, 939, 416, 472, 5462, 3230, 4488, 5286, 24861, 32370, 38540, 21742],
+        // }
       ]
     }
   }
