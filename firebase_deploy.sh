5922f14d53ff35620df30d12b2d2aac1083e15ab
James Morgan <james.morgan.e@googlemail.com>
2020-09-03 08:31:16 +0100

    Moving insights to ko FB

--- firebase_deploy.sh
+++ firebase_deploy.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
-# Version key/value should be on his own line
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-
 npm run build;
 
-echo "Deploying version $PACKAGE_VERSION to PRODUCTION"
-firebase use known-origin-dashboard;
-firebase deploy --only hosting:known-origin-dashboard;
+echo "Deploying insights"
 
+firebase use known-origin-io;
+firebase deploy --only hosting:known-origin-insights;
